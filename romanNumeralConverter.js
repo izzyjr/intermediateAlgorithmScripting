c6132e2f0b4df142c3ed77fb3cb721f9a9f14a04
Israel Mesa <mesa.israel50@gmail.com>
2017-10-21 22:29:04 +0000

    final update

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,10 +1,17 @@
 function convertToRoman(num) {
-var roman = ['I', 'IV', 'V', 'IX', 'X'];
-var decimal = [1, 4, 5, 9, 10];
+    var decimal = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    var roman = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
 
+  var asRoman = '';
 
+  for (var i = 0; i < decimal.length; i++) {
+    while (decimal[i] <= num) {
+      asRoman = asRoman + roman[i];
+      num = num - decimal[i];
+    }
+  }
 
-
+  console.log(asRoman);
 }
 
-convertToRoman(6);
+convertToRoman(2000);
